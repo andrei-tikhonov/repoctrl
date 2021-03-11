850eb74740261263d2d80d27e76ea1c138afadff
Andrei_Tikhonov <andrei_tikhonov@epam.com>
2021-03-11 10:32:41 +0400

    Add basic router

---
+++ src/router/router.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route,Link } from "react-router-dom";
+import {routes} from "./routes";
+
+export const Router = () => {
+  return (
+      <BrowserRouter>
+              <nav>
+                  <ul>
+                      <li>
+                          <Link to={routes.repositories}>Repositories</Link>
+                      </li>
+                      <li>
+                          <Link to={routes.pullRequests}>Pull requests</Link>
+                      </li>
+                  </ul>
+              </nav>
+
+              <Switch>
+                  <Route path={routes.pullRequests}>
+                      Pull requests
+                  </Route>
+                  <Route path={routes.repositories}>
+                      Repositories
+                  </Route>
+              </Switch>
+      </BrowserRouter>
+  );
+}
